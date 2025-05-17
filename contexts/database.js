1a5bbb37e51fc2098cbe34a6fd02b15278a31357
rjmango <you@example.comgit config --global user.email you@example.com>
2025-05-17 15:08:39 +0800

    Connected upload clothing image to database

--- contexts/database.js
+++ contexts/database.js
@@ -7,7 +7,32 @@ const databaseID = "67ad9e670028ece6ed36";
 const userCollectionID = "67d3ea200018791dcc14";
 const categoryCollectionID = "67d3c85800348c24a27b";
 const avatarCollectionID = "67d3c8cd002354dcef59";
-const storageID = "6825d9f500066a3dc28e"; // Storage ID
+const clothingCollectionID = "67d3c99a0027096bb0c2";
+
+const avatarStorageID = "6825d9f500066a3dc28e"; // Storage ID
+const clothesStorageID = "6828105b000b23c42ebe"; // Clothes Storage ID
+
+const addClothingImage = async ( filePath ) =>  {
+  const id = ID.unique();
+  console.log(filePath);
+  try {
+    const result = await storage.createFile(
+      clothesStorageID, // bucketId
+      id, // fileId
+      {
+        name: 'clothing.jpg',
+        type: 'image/jpg',
+        size: 1234567,
+        uri: filePath
+      }, // file
+    );
+    console.log(result); // Success
+    return id;
+  } catch (error) {
+    console.error(error); // Failure
+    return null;
+  }
+}
 
 const addUserAvatar = async ( filePath ) => {
   const id = ID.unique();
@@ -28,6 +53,21 @@ const addUserAvatar = async ( filePath ) => {
   } catch (error) {
     console.error(error); // Failure
     return null;
+  }
+}
+
+const addClothingDocument = async ( data ) => {
+  try {
+    const response = await databases.createDocument(
+      databaseID,
+      clothingCollectionID,
+      "unique()", // Auto-generate document ID
+      data
+    );
+    console.log("Document added:", response);
+    return response;
+  } catch (error) {
+    console.error("Error adding document:", error);
   }
 }
 
@@ -96,4 +136,4 @@ async function getCategoryDocumentsByUserId(targetUserId) {
   }
 }
 
-export { addUserDocument, addCategoryDocument, getCategoryDocumentsByUserId, addAvatarDocument, addUserAvatar };
+export { addUserDocument, addCategoryDocument, addClothingDocument, getCategoryDocumentsByUserId, addAvatarDocument, addUserAvatar, addClothingImage };
